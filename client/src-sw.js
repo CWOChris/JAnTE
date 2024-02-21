328cf58ab3226388a66fbf919ec9a4f04d93bd67
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-21 17:09:39 -0600

    added the service workers js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,67 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+// const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+// const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+registerRoute({
+  cacheName: 'html-cache',
   plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxEntries: 10,
+      maxAgeSeconds: 7 * 24 * 60 * 60,
     }),
   ],
 });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
+registerRoute(
+  /\.(?:css|js|png|jpg|jpeg|svg|gif)$/,
+  new CacheFirst({
+    cacheName: 'static-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(
+  ({ request }) => request.destination !== 'document',
+  new CacheFirst({
+    cacheName: 'other-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
 
-// TODO: Implement asset caching
-registerRoute();
+// Starter code below
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
+
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
+
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// // TODO: Implement asset caching
+// registerRoute();
